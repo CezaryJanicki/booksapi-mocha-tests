72186bb6ee5a3cb0f2b0c3653c6d09fc3938f30a
CezaryJanicki <cezary.janicki@gmail.com>
2023-11-24 13:00:41 +0100

    Mocha tests

---
+++ utils/formatFullname.js
@@ -0,0 +1,9 @@
+module.exports = (fullName) => {
+  if (typeof fullName !== 'string' || fullName.trim() === '') return 'Error';
+  const nameParts = fullName.trim().split(/\s+/);
+  if(nameParts.length > 2 || nameParts.length === 0) return 'Error';
+  if(nameParts.length === 1) return `${nameParts[0].charAt(0).toUpperCase()}${nameParts[0].slice(1).toLowerCase()}`;
+  const [ firstName, lastName ] = nameParts;
+  if (!/^[a-zA-Z]+$/.test(firstName) || !/^[a-zA-Z]+$/.test(lastName)) return 'Error';
+  return `${lastName.charAt(0).toUpperCase()}${lastName.slice(1).toLowerCase()}, ${firstName.charAt(0).toUpperCase()}${firstName.slice(1).toLowerCase()}`;
+}
---
+++ utils/test/formatFullname.test.js
@@ -0,0 +1,86 @@
+const formatFullname = require('../formatFullname.js');
+const expect = require('chai').expect;
+
+describe('formatFullname', () => {
+  it('should return formatted full name when given a valid full name', () => {
+    const fullName = 'John Doe';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return "error" when given an empty full name', () => {
+    const fullName = '';
+    const expected = 'Error';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the last name when given a single name', () => {
+    const fullName = 'Smith';
+    const expected = 'Smith';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the full name when given a name with leading/trailing spaces', () => {
+    const fullName = '   John Doe   ';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the full name when given a name with extra spaces between first and last name', () => {
+    const fullName = 'John     Doe';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the full name when given a name with multiple spaces between first and last name', () => {
+    const fullName = 'John        Doe';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the full name when given a name with uppercase letters', () => {
+    const fullName = 'JOHN DOE';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return the full name when given a name with lowercase letters', () => {
+    const fullName = 'john doe';
+    const expected = 'Doe, John';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return "Error" when given no name', () => {
+    const fullName = '';
+    const expected = 'Error';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return "Error" when given a non-string input', () => {
+    const fullName = 123;
+    const expected = 'Error';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+  it('should return "Error" when given a name with additional text after the last name', () => {
+    const fullName = 'John Doe Test';
+    const expected = 'Error';
+    const result = formatFullname(fullName);
+    expect(result).to.equal(expected);
+  });
+
+});
+
+
+
+
